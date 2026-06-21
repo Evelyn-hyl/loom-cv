075bab199e778ef7ff277064586ef3f7673590e6
Evelyn-hyl <evvvta@gmail.com>
2026-06-21 16:21:18 -0700

    docs: add param descriptions to edge detection and vanishing point headers

--- engine/include/edge_detection.hpp
+++ engine/include/edge_detection.hpp
@@ -3,11 +3,23 @@
 #include <opencv2/core.hpp>
 #include <vector>
 
-std::vector<cv::Vec4i> detectLines(
-    const cv::Mat& image, 
-    double lowThreshold = 40, 
-    double highThreshold = 100,
-    int isALineThreshold = 150,
-    double minLineLength = 150,
-    double maxLineGap = 10
-);
+/**
+ * Detects all line segments in the image
+ * 
+ * @param image A BGR color image
+ * 
+ * @return An array of line segments
+ */
+std::vector<cv::Vec4i> detectLines(const cv::Mat& image);
+
+/**
+ * Filters out vertical/horizontal line segments
+ * @note May be removed with the introduction of 3-point perspective analysis
+ * 
+ * @param lines An array of line segments
+ * @param angleThresholdDeg An angle measured relative to 0°, 90°, and 180°; 
+ * when angle is exactly at the threshold, it is considered a vertical/horizontal line and filtered out
+ * 
+ * @return An array of non-vertical/horizontal line segments
+ */
+std::vector<cv::Vec4i> filterDiagonalLines(const std::vector<cv::Vec4i>& lines, double angleThresholdDeg = 5.0);
--- engine/include/vanishing_point.hpp
+++ engine/include/vanishing_point.hpp
@@ -13,8 +13,20 @@
  * 4. Get the best vanishing point, which has the most inlier lines
  * 5. Repeat for a second vanishing point
  * 
- * @param lines
+ * @param lines An array of line segments
  * 
  * @return An array of vanishing points
  */
 std::vector<cv::Point2d> detectVanishingPoints(const std::vector<cv::Vec4i>& lines);
+
+/**
+ * Retrieves all lines that align with the given vanishing point
+ * 
+ * @param lines An array of line segments
+ * @param vp A vanishing point
+ * @param angleThresholdDeg The maximum angular deviation (in degrees) 
+ * between a segment's direction and the direction toward the vanishing point
+ * 
+ * @return An array of inlier line segments
+ */
+std::vector<cv::Vec4i> getInlierLines(const std::vector<cv::Vec4i>& lines, cv::Point2d vp, double angleThresholdDeg = 10.0);
